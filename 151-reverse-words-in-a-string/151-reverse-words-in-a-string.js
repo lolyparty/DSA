cc98b51bee2811d36291b6d72c5d717ee868c4ac
Ololade <49575651+lolyparty@users.noreply.github.com>
2022-05-29 20:02:54 +0100

    Time: 70 ms (78.63%), Space: 43.7 MB (86.27%) - LeetHub

---
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    const newArr = s.trim().split(' ').reverse()
+    let newString = ''
+    for(let i = 0; i < newArr.length; i++){
+        if(newArr[i] !== ""){
+            newString = `${newString} ${newArr[i]}`
+        }
+    }
+    return newString.trim()
+};
