66675942d1ef22f4782ccf3e05ffe54638e7a270
Ololade <49575651+lolyparty@users.noreply.github.com>
2022-06-08 23:00:00 +0100

    Time: 76 ms (89.91%), Space: 44.8 MB (55.92%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -4,12 +4,15 @@
  */
 var removeDuplicates = function(nums) {
     const repeatedNums = []
+    let curIndex = 1
+    
      for(let i =0; i < nums.length; i++){
          if(repeatedNums.indexOf(nums[i]) === -1){
              repeatedNums.push(nums[i])
+             
          }
      }
     
-    nums.unshift(...repeatedNums)
+    nums.splice(0, 0, ...repeatedNums)
     return repeatedNums.length
 };
