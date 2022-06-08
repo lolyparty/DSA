ca377a62d228347070edc7ad243d879f5b4fe448
Ololade <49575651+lolyparty@users.noreply.github.com>
2022-06-08 02:17:37 +0100

    Time: 96 ms (34.93%), Space: 42.6 MB (87.33%) - LeetHub

---
+++ 804-unique-morse-code-words/804-unique-morse-code-words.js
@@ -0,0 +1,52 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var uniqueMorseRepresentations = function(words) {
+        const morseCode = {
+            'a':".-",
+            'b':"-...",
+            'c':"-.-.",
+            'd':"-..",
+            'e':".",
+            'f':"..-.",
+            'g':"--.",
+            'h':"....",
+            'i':"..",
+            'j':".---",
+            'k':"-.-",
+            'l':".-..",
+            'm':"--",
+            'n':"-.",
+            'o':"---",
+            'p':".--.",
+            'q':"--.-",
+            'r':".-.",
+            's':"...",
+            't':"-",
+            'u':"..-",
+            'v':"...-",
+            'w':".--",
+            'x':"-..-",
+            'y':"-.--",
+            'z':"--.."   
+        }
+        
+        let wordMorseCode = ''
+        let wordMorseCodeArr = []
+        
+        for (let i = 0; i < words.length; i++){
+            let curWord = words[i].split('')
+            for(let j = 0; j < curWord.length; j++){
+                wordMorseCode = `${wordMorseCode}${morseCode[curWord[j]]}`
+                wordMorseCode = wordMorseCode.trim()
+            }
+            if(wordMorseCodeArr.indexOf(wordMorseCode) === -1){
+                    wordMorseCodeArr.push(wordMorseCode)
+                }
+            wordMorseCode = ''
+            
+            
+        }
+        return wordMorseCodeArr.length
+    }
