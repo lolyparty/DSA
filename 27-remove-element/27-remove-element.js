a2054d6e86cd33b1ef647eeccd8a062c0005b270
Ololade <49575651+lolyparty@users.noreply.github.com>
2022-06-23 04:21:12 +0100

    Time: 88 ms (45.03%), Space: 41.6 MB (92.51%) - LeetHub

---
+++ 27-remove-element/27-remove-element.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let start = 0
+    
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] !== val){
+            nums[start] = nums[i];
+            start++;
+        }
+    }
+    
+    return start;
+};
