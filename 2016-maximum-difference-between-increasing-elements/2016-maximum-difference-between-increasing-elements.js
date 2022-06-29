8ce1f867fee8fb61924c015d933a0d8171e77210
Ololade <49575651+lolyparty@users.noreply.github.com>
2022-06-29 23:56:53 +0100

    Time: 74 ms (74.91%), Space: 41.8 MB (85.57%) - LeetHub

--- 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.js
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.js
@@ -4,13 +4,14 @@
  */
 var maximumDifference = function(nums) {
     let difference = 0;
+    let minValue = Math.max(...nums)
     
     for(let i = 0; i < nums.length; i++){
-        for(let j = i + 1; j < nums.length; j++){
-            if(nums[j] - nums[i] > difference){
-                difference = nums[j] - nums[i]
-            }
+        if(nums[i] < minValue){
+            minValue = nums[i]
+        } else if(nums[i] - minValue > difference){
+            difference = nums[i] - minValue;
         }
     }
-    return difference ? difference : -1;
+    return !!difference ? difference : -1;
 };
