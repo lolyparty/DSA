3eb0887fdd25804a7b9d083ed69b1b1dd2e6f8ca
Ololade <49575651+lolyparty@users.noreply.github.com>
2022-06-30 23:58:37 +0100

    Time: 132 ms (5.33%), Space: 42.7 MB (13.12%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -14,12 +14,13 @@ var searchInsert = function(nums, target) {
     let start = 0;
     let end = nums.length - 1;
     while (start <= end){
-        let mid = start + Math.floor((end - start)/2);
-        if(nums[mid] === target) return mid;
-        else if(nums[mid] > target){
-            end = mid - 1;
+        mid = Math.floor((start + end)/2)
+        if(nums[mid] < target){
+            start++
+        } else if(nums[mid] > target){
+            end--
         } else{
-            start = mid + 1;
+            return mid
         }
     }
     return start;
