e786abf73c3876b56a762b5ac50f4f99494ebea1
Ololade <49575651+lolyparty@users.noreply.github.com>
2023-04-16 00:37:36 +0100

    Time: 698 ms (18.75%), Space: 45 MB (16.67%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.js
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.js
@@ -7,27 +7,13 @@ var minOperations = function(boxes) {
     var outPut = [];
     
     for (var i = 0; i < boxes.length; i++){
-        var moves = 0;
-        
-        if(i > 0){
-            for(var j = i - 1; j > -1; j--){
-                if(boxes[j] > 0){
-                    moves = moves + ((i - j) *  boxes[j])
-                }
-            }
-            for(var j = i + 1; j < boxes.length; j++){
-                if(boxes[j] > 0){
-                    moves = moves + ((j - i) *  boxes[j])
-                }
-            }
-        } else {
-            for(var j = i + 1; j < boxes.length; j++){
-                if(boxes[j] > 0){
-                    moves = moves + ((j - i) *  boxes[j])
-                }
-            }
-        }       
-        outPut.push(moves)
+    var moves = 0;
+    for (var j = 0; j < boxes.length; j++) {
+        if (boxes[j] > 0) {
+            moves += Math.abs(i - j) * boxes[j];
+        }
     }
+    outPut.push(moves);
+}
     return outPut;
 };
